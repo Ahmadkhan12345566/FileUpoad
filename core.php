e828b9b1e1136fbfbddd5647054ba42b1c56df58
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-07-05 00:51:28 -0700

    savae data to csv file

--- core.php
+++ core.php
@@ -4,18 +4,17 @@ $dirNamesAll = array();
 $dirNames = array();
 $files_to_return = array();
 
-$action=$_POST['postaction'];
-if($action=="main"){
-    $path=$_POST['postname'];
-    Core::copydir($path,'raw2');
+$action = $_POST['postaction'];
+if ($action == "main") {
+    $path = $_POST['postname'];
+    Core::copydir($path, 'raw2');
     $dirNames = Core::readFiles('raw2');
-    echo "raw2/",$dirNames[0];
-}
-elseif($action=="next"){
-    $path=$_POST['postname'];
-    $imgpath=$_POST['postimgname'];
-    $imgName=substr($imgpath,5,strlen($imgpath));
-    $dirNamesAll= Core::readFiles($path);
+    echo "raw2/", $dirNames[0];
+} elseif ($action == "next") {
+    $path = $_POST['postname'];
+    $imgpath = $_POST['postimgname'];
+    $imgName = substr($imgpath, 5, strlen($imgpath));
+    $dirNamesAll = Core::readFiles($path);
     $key = array_search($imgName, $dirNamesAll);
     /* if($key+2==sizeof($dirNamesAll)) {
             Core::copyFile($path, 'raw2', $dirNamesAll[$key + 1]);
@@ -23,33 +22,30 @@ elseif($action=="next"){
             echo "raw2/", $dirNames[0];
         }
         else{*/
-            Core::copyFile($path, 'raw2', $dirNamesAll[$key + 1]);
-            $dirNames = Core::readFiles('raw2');
-            echo "raw2/", $dirNames[0];
-        //}
+    Core::copyFile($path, 'raw2', $dirNamesAll[$key + 1]);
+    $dirNames = Core::readFiles('raw2');
+    echo "raw2/", $dirNames[0];
+    //}
 
 
-}
-elseif ($action=="previous"){
-    $path=$_POST['postname'];
-    $imgpath=$_POST['postimgname'];
-    $imgName=substr($imgpath,5,strlen($imgpath));
-    $dirNamesAll= Core::readFiles($path);
+} elseif ($action == "previous") {
+    $path = $_POST['postname'];
+    $imgpath = $_POST['postimgname'];
+    $imgName = substr($imgpath, 5, strlen($imgpath));
+    $dirNamesAll = Core::readFiles($path);
     $key = array_search($imgName, $dirNamesAll);
     Core::copyFile($path, 'raw2', $dirNamesAll[$key - 1]);
     $dirNames = Core::readFiles('raw2');
     echo "raw2/", $dirNames[0];
-}
-elseif ($action=="writefile"){
+} elseif ($action == "writefile") {
 
-    $filNam =$_POST['postimgname'];
-    $filename=substr($filNam,5,-4);;
-    $name=$_POST['postname'];
-    $fathername=$_POST['postfathername'];
-    $cnic=$_POST['postcnic'];
-    Core::writeFile($filename,$name,$fathername,$cnic);
-}
-else{
+    $filNam = $_POST['postimgname'];
+    $filename = substr($filNam, 5, -4);;
+    $name = $_POST['postname'];
+    $fathername = $_POST['postfathername'];
+    $cnic = $_POST['postcnic'];
+    Core::writeFile($filename, $name, $fathername, $cnic);
+} else {
     echo "Action Not Found";
 }
 
@@ -64,6 +60,7 @@ class Core
             'faqs' => 'faq.php',
             'contactus' => 'contact.php',
             'core' => 'core.php');
+
     public static function readFiles($arg)
     {
         $files = array();
@@ -88,42 +85,43 @@ class Core
         }
         return $files;
     }
-    public static function copydir($source,$destination)
+
+    public static function copydir($source, $destination)
     {
-       self::deleteFile();
-        if(!is_dir($destination)){
+        self::deleteFile();
+        if (!is_dir($destination)) {
             $oldumask = umask(0);
             mkdir($destination, 01777); // so you get the sticky bit set
             umask($oldumask);
         }
         $dir_handle = @opendir($source) or die("Unable to open");
-        while ($file = readdir($dir_handle))
-        {
-            if($file!="." && $file!=".." && !is_dir("$source/$file"))
-                if( strtolower(substr($file, strrpos($file, '.') + 1)) == 'jpg' || strtolower(substr($file, strrpos($file, '.') + 1)) == 'png') {
+        while ($file = readdir($dir_handle)) {
+            if ($file != "." && $file != ".." && !is_dir("$source/$file"))
+                if (strtolower(substr($file, strrpos($file, '.') + 1)) == 'jpg' || strtolower(substr($file, strrpos($file, '.') + 1)) == 'png') {
 
-                    self::copyFile($source,$destination,$file);
+                    self::copyFile($source, $destination, $file);
                     break;
-                //     copy("$source/$file", "$destination/$file");
+                    //     copy("$source/$file", "$destination/$file");
                 }
         }
         closedir($dir_handle);
     }
 
 
-    public static function deleteFile(){
+    public static function deleteFile()
+    {
         $files = glob('raw2/*');
-        foreach($files as $file){ // iterate files
-            if(is_file($file))
+        foreach ($files as $file) { // iterate files
+            if (is_file($file))
                 unlink($file); // delete file
         }
     }
 
 
-    public static function copyFile($source,$destination,$file)
+    public static function copyFile($source, $destination, $file)
     {
         self::deleteFile();
-        if(!is_dir($destination)){
+        if (!is_dir($destination)) {
             $oldumask = umask(0);
             mkdir($destination, 01777); // so you get the sticky bit set
             umask($oldumask);
@@ -131,35 +129,32 @@ class Core
         $dir_handle = @opendir($source) or die("Unable to open");
         //while ($file = readdir($dir_handle))
         //{
-            if($file!="." && $file!=".." && !is_dir("$source/$file"))
-                if( strtolower(substr($file, strrpos($file, '.') + 1)) == 'jpg' || strtolower(substr($file, strrpos($file, '.') + 1)) == 'png') {
-                    copy("$source/$file", "$destination/$file");
-                }
+        if ($file != "." && $file != ".." && !is_dir("$source/$file"))
+            if (strtolower(substr($file, strrpos($file, '.') + 1)) == 'jpg' || strtolower(substr($file, strrpos($file, '.') + 1)) == 'png') {
+                copy("$source/$file", "$destination/$file");
+            }
         //}
         closedir($dir_handle);
     }
 
-    public static function writeFile($fileName,$Name,$Fathername,$cnic){
+    public static function writeFile($fileName, $Name, $Fathername, $cnic)
+    {
         $dir_path = "raw/";
-        $ourFileName = $dir_path.$fileName.".txt";
-        $ourFileHandle = fopen($ourFileName, 'w') or die("can't open file");
-        fwrite($ourFileHandle, "Name:  ");
-        fwrite($ourFileHandle, $Name.PHP_EOL);
+        $ourFileName = $dir_path . $fileName . ".csv";
+        $ourFileHandle = fopen($ourFileName, 'a+') or die("can't open file");
+        $a=array($Name,$Fathername,$cnic."-",);
+        fputcsv($ourFileHandle, $a);
+     /*   fwrite($ourFileHandle, "Name:  ");
+        fwrite($ourFileHandle, $Name . PHP_EOL);
         fwrite($ourFileHandle, nl2br("Father Name: "));
-        fwrite($ourFileHandle, $Fathername.PHP_EOL);
+        fwrite($ourFileHandle, $Fathername . PHP_EOL);
         fwrite($ourFileHandle, "Cnic:  ");
-        fwrite($ourFileHandle, $cnic);
-        echo "done";
+        fwrite($ourFileHandle, $cnic);*/
+        echo "done"+$cnic;
     }
 
 
-
-
-
 }
-
-
-
 
 
 ?>
