f4a486bcb1a18936a024f56df767eaea21310d0f
raoasifraza11 <raoasifraza1@hotmail.com>
2019-01-26 23:47:24 +0500

    Fix and merge

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">    
+    <link rel="stylesheet" type="text/css" href="vs/twbs/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="CSS/style.css">
     <style>
         #loader {
             display: none;
